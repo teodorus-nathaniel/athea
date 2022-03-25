bf9271a73c6a1cd3a682c390ecd885ed653b62af
Teodorus Nathaniel <nathankurnia26@gmail.com>
2022-03-25 17:12:33 +0700

    Extract navbar overlay

--- src/components/Navbar/NavbarContent.tsx
+++ src/components/Navbar/NavbarContent.tsx
@@ -13,10 +13,6 @@ type Props = {
   isOpen: boolean
 }
 
-const containerVariants: TransitionVariants = {
-  close: { opacity: 0, y: -50 },
-  open: { opacity: 1, y: 0 },
-}
 const contentVariants: TransitionVariants = {
   close: { opacity: 0, transition: { ...NORMAL_TRANSITION } },
   open: {
@@ -31,27 +27,16 @@ const contentVariants: TransitionVariants = {
 export default function NavbarContent({ isOpen }: Props) {
   return (
     <>
+      <NavbarOverlay isOpen={isOpen} />
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            variants={containerVariants}
-            exit='close'
-            animate='open'
-            initial='close'
-            transition={SLOW_TRANSITION}
             className={clsx(
               'fixed top-0 left-0',
-              'w-full h-screen',
-              'bg-black text-white',
-              'z-20'
+              'pt-24 px-4 w-full',
+              'z-20',
+              'flex flex-col'
             )}
-          />
-        )}
-      </AnimatePresence>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            className={clsx('fixed top-0 left-0', 'pt-24 px-4', 'z-20')}
             variants={contentVariants}
             initial='close'
             animate='open'
@@ -61,5 +46,31 @@ export default function NavbarContent({ isOpen }: Props) {
         )}
       </AnimatePresence>
     </>
+  )
+}
+
+const overlayVariants: TransitionVariants = {
+  close: { opacity: 0, y: -50 },
+  open: { opacity: 1, y: 0 },
+}
+function NavbarOverlay({ isOpen }: Props) {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          variants={overlayVariants}
+          exit='close'
+          animate='open'
+          initial='close'
+          transition={SLOW_TRANSITION}
+          className={clsx(
+            'fixed top-0 left-0',
+            'w-full h-screen',
+            'bg-black text-white',
+            'z-20'
+          )}
+        />
+      )}
+    </AnimatePresence>
   )
 }
