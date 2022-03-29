4642070d4e7e5ace212a1f2df54d38d2747d8bfa
Teodorus Nathaniel <nathankurnia26@gmail.com>
2022-03-30 00:12:14 +0700

    Remove unneeded contact info in services

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,13 +1,9 @@
 import Contact from '#/components/Contact'
 import Layout from '#/components/layouts/Layout'
-import ProjectOverviewList from '#/components/ProjectOverviewList'
 import HeadlineSection from '#/components/sections/HeadlineSection'
 import SectionWrapper from '#/components/sections/SectionWrapper'
 import ServicesList from '#/components/ServicesList'
-import recentWorks from '#/data/recent-works'
 import services from '#/data/services'
-import Button from '#/ui/Button'
-import clsx from 'clsx'
 import type { NextPage } from 'next'
 
 const Services: NextPage = () => {
@@ -19,18 +15,11 @@ const Services: NextPage = () => {
         subheading='Not your typical agency. We are your next set of creative innovation experts.\nAthea is a multidisciplinary creative company. We are based in Jakarta and founded in 2019 had 4 subsidiary based on functionality'
         type='souls'
       />
-      <SectionWrapper title='Recent Works'>
-        <ProjectOverviewList projects={recentWorks} />
-        <Button className={clsx('mt-8')}>Discover More</Button>
-      </SectionWrapper>
       <SectionWrapper className='px-0' title='Our Services'>
         <ServicesList services={services} />
       </SectionWrapper>
       <SectionWrapper title='Contact Us'>
-        <Contact
-          displayedSections={['contact', 'address', 'socialMedia']}
-          className='w-full'
-        />
+        <Contact displayedSections={['contact']} className='w-full' />
       </SectionWrapper>
     </Layout>
   )
