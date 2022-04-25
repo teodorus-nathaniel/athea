5e16d6be99fbce578eb35dc50a0ab4e279d004a4
Teodorus Nathaniel <nathankurnia26@gmail.com>
2022-04-25 16:51:26 +0700

    Disable link in nav link animation state

--- src/components/Navbar/NavbarContent/NavbarContentDesktop.tsx
+++ src/components/Navbar/NavbarContent/NavbarContentDesktop.tsx
@@ -51,6 +51,14 @@ export default function NavbarContentDesktop({
     return linksCopy
   }, [links])
 
+  const toggleNavClick = (status: 'disable' | 'enable') => {
+    const navLinks = document.getElementsByClassName('nav-link')
+    Array.from(navLinks).forEach((link) => {
+      const castedLink = link as HTMLAnchorElement
+      castedLink.style.pointerEvents = status === 'disable' ? 'none' : 'auto'
+    })
+  }
+
   return (
     <AnimatePresence>
       {isOpen && (
@@ -59,6 +67,8 @@ export default function NavbarContentDesktop({
           initial='init'
           animate='open'
           exit='close'
+          onAnimationComplete={() => toggleNavClick('enable')}
+          onAnimationStart={() => toggleNavClick('disable')}
           className={clsx(
             'mr-6 mt-8',
             'fixed top-0 right-0 z-30',
@@ -70,7 +80,7 @@ export default function NavbarContentDesktop({
               <motion.div
                 variants={generateContentVariants(idx)}
                 key={text}
-                className={clsx('absolute top-1/2 right-0')}
+                className={clsx('nav-link', 'absolute top-1/2 right-0')}
                 transition={NORMAL_TRANSITION}>
                 <NavbarLink
                   href={href}
