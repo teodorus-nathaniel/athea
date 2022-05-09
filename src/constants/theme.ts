97028b129b9f11bb8db5680b0da03445b0b0d505
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-05-09 16:09:35 +0700

    Set text white for dark theme

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 export type ThemeTypes = 'light' | 'dark'
 export const themeClassNames: { [key in ThemeTypes]: string } = {
   light: clsx('text-black bg-white'),
-  dark: clsx('text-gray-300 bg-black'),
+  dark: clsx('text-white bg-black'),
 }
 
 export const breakpoints = ['sm', 'md', 'lg', 'xl', '2xl'] as const
