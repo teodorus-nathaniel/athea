90cc026a0ff492d23d6968761f8815a19a3289f9
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-05-10 20:32:06 +0700

    Flexible snap to data

--- src/modules/services/ServicesDesktop/ServicesListSection/ServicesListSection.tsx
+++ src/modules/services/ServicesDesktop/ServicesListSection/ServicesListSection.tsx
@@ -5,25 +5,35 @@ import services from '#/data/services'
 import { ScrollTriggerProvider } from '#/helpers/contexts/ScrollTriggerProvider'
 import Text from '#/ui/Text'
 import { linear } from '@popmotion/easing'
+import clsx from 'clsx'
 import React, { useMemo } from 'react'
 import ServicesListContent from './ServicesListContent'
 
 export default function ServicesListSection(props: SectionWrapperProps) {
-  const options = useMemo(
-    () => ({
+  const options = useMemo(() => {
+    const snapTo = Array.from({ length: services.length }).map(
+      (_, idx) => (1 / (services.length - 1)) * idx
+    )
+
+    return {
       snap: {
         delay: 0,
-        snapTo: [0, 0.3333, 0.6666, 1],
-        duration: 0.3,
+        snapTo,
+        duration: 0.25,
         ease: linear,
       },
-    }),
-    []
-  )
+    }
+  }, [])
 
   return (
     <ScrollTriggerProvider options={options}>
       <SectionWrapper
+        className={clsx(
+          'min-h-screen',
+          'flex items-center',
+          '!py-8',
+          'justify-center'
+        )}
         title={
           <>
             <Text serif bold>
