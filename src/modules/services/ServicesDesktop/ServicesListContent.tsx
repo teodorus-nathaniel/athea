0aea06bddfd1958fe246bffee79bc9a1dd3994db
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-05-10 13:33:33 +0700

    Use sizeme for better way to get content height

--- src/modules/services/ServicesDesktop/ServicesListContent.tsx
+++ src/modules/services/ServicesDesktop/ServicesListContent.tsx
@@ -7,26 +7,13 @@ import { useScrollTriggerContext } from '#/helpers/contexts/ScrollTriggerProvide
 import Text from '#/ui/Text'
 import clsx from 'clsx'
 import { motion } from 'framer-motion'
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React from 'react'
+import { SizeMe } from 'react-sizeme'
 import ServiceAnimated from './ServiceAnimated'
 
+const OFFSET = 100
 export default function ServicesListContent(props: SectionWrapperProps) {
   const progress = useScrollTriggerContext()
-
-  const [contentRect, setContentRect] = useState({ width: 0, height: 0 })
-  const dummyContentRef = useRef<HTMLDivElement | null>(null)
-  useLayoutEffect(() => {
-    // TODO: check better way
-    setTimeout(() => {
-      if (!dummyContentRef.current) return
-      const contentBoundingRect =
-        dummyContentRef.current.getBoundingClientRect()
-      setContentRect(contentBoundingRect)
-    }, 100)
-  }, [])
-
-  const { height } = contentRect
-  const heightAndOffset = height + 100
 
   return (
     <SectionWrapper
@@ -41,38 +28,42 @@ export default function ServicesListContent(props: SectionWrapperProps) {
       {...props}>
       <div className={clsx('flex items-stretch', 'w-full')}>
         <div className={clsx('w-8', 'bg-red-500')}></div>
-        <div
-          className={clsx('relative', 'ml-16', 'flex-1')}
-          style={{ perspective: '2000px' }}>
-          <div ref={dummyContentRef}>
-            <Service
-              className='opacity-0 pointer-events-none'
-              subtitle='dummy'
-              title='dummy'
-              image=''
-              number={1}
-            />
-          </div>
-
-          <motion.div
-            className={clsx(
-              'absolute top-0 left-0',
-              'w-full h-full',
-              'overflow-hidden'
-            )}>
-            {progress &&
-              services.map((service, idx) => (
-                <ServiceAnimated
-                  idx={idx}
-                  length={services.length}
-                  service={service}
-                  progress={progress}
-                  key={idx}
-                  offset={heightAndOffset}
+        <SizeMe monitorHeight monitorWidth={false}>
+          {({ size: { height } }) => (
+            <div
+              className={clsx('relative', 'ml-16', 'flex-1')}
+              style={{ perspective: '2000px' }}>
+              <div>
+                <Service
+                  className='opacity-0 pointer-events-none'
+                  subtitle='dummy'
+                  title='dummy'
+                  image=''
+                  number={1}
                 />
-              ))}
-          </motion.div>
-        </div>
+              </div>
+
+              <motion.div
+                className={clsx(
+                  'absolute top-0 left-0',
+                  'w-full h-full',
+                  'overflow-hidden'
+                )}>
+                {progress &&
+                  services.map((service, idx) => (
+                    <ServiceAnimated
+                      idx={idx}
+                      length={services.length}
+                      service={service}
+                      progress={progress}
+                      key={idx}
+                      offset={(height ?? 0) + OFFSET}
+                    />
+                  ))}
+              </motion.div>
+            </div>
+          )}
+        </SizeMe>
       </div>
     </SectionWrapper>
   )
