6477c9053332f96beea8d62c2e818173cc15d2cf
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-05-19 22:10:33 +0700

    Add video shadow

---
+++ src/helpers/instrinsic-sizes.ts
@@ -0,0 +1,114 @@
+export function calculateObjectFitElement(
+  element: HTMLImageElement | HTMLVideoElement
+) {
+  const imageComputedStyle = window.getComputedStyle(element)
+  const imageObjectFit = imageComputedStyle.getPropertyValue('object-fit')
+  const coordinates: {
+    sourceWidth?: number
+    sourceHeight?: number
+    sourceX?: number
+    sourceY?: number
+    width?: number
+    height?: number
+    destinationWidthPercentage?: number
+    destinationHeightPercentage?: number
+    destinationXPercentage?: number
+    destinationYPercentage?: number
+  } = {}
+  const imagePositions = imageComputedStyle
+    .getPropertyValue('object-position')
+    .split(' ')
+
+  let naturalWidth
+  let naturalHeight
+  if (element.tagName === 'VIDEO') {
+    naturalWidth = (element as HTMLVideoElement).videoWidth
+    naturalHeight = (element as HTMLVideoElement).videoHeight
+  } else {
+    naturalWidth = (element as HTMLImageElement).naturalWidth
+    naturalHeight = (element as HTMLImageElement).naturalHeight
+  }
+
+  const horizontalPercentage = parseInt(imagePositions[0]) / 100
+  const verticalPercentage = parseInt(imagePositions[1]) / 100
+  const naturalRatio = naturalWidth / naturalHeight
+  const visibleRatio = element.clientWidth / element.clientHeight
+  if (imageObjectFit === 'none') {
+    coordinates.sourceWidth = element.clientWidth
+    coordinates.sourceHeight = element.clientHeight
+    coordinates.sourceX =
+      (naturalWidth - element.clientWidth) * horizontalPercentage
+    coordinates.sourceY =
+      (naturalHeight - element.clientHeight) * verticalPercentage
+    coordinates.destinationWidthPercentage = 1
+    coordinates.destinationHeightPercentage = 1
+    coordinates.destinationXPercentage = 0
+    coordinates.destinationYPercentage = 0
+  } else if (imageObjectFit === 'contain' || imageObjectFit === 'scale-down') {
+    coordinates.sourceWidth = naturalWidth
+    coordinates.sourceHeight = naturalHeight
+    coordinates.sourceX = 0
+    coordinates.sourceY = 0
+    if (naturalRatio > visibleRatio) {
+      coordinates.destinationWidthPercentage = 1
+      coordinates.destinationHeightPercentage =
+        naturalHeight /
+        element.clientHeight /
+        (naturalWidth / element.clientWidth)
+      coordinates.destinationXPercentage = 0
+      coordinates.destinationYPercentage =
+        (1 - coordinates.destinationHeightPercentage) * verticalPercentage
+    } else {
+      coordinates.destinationWidthPercentage =
+        naturalWidth /
+        element.clientWidth /
+        (naturalHeight / element.clientHeight)
+      coordinates.destinationHeightPercentage = 1
+      coordinates.destinationXPercentage =
+        (1 - coordinates.destinationWidthPercentage) * horizontalPercentage
+      coordinates.destinationYPercentage = 0
+    }
+  } else if (imageObjectFit === 'cover') {
+    if (naturalRatio > visibleRatio) {
+      coordinates.sourceWidth = naturalHeight * visibleRatio
+      coordinates.sourceHeight = naturalHeight
+      coordinates.sourceX =
+        (naturalWidth - coordinates.sourceWidth) * horizontalPercentage
+      coordinates.sourceY = 0
+    } else {
+      coordinates.sourceWidth = naturalWidth
+      coordinates.sourceHeight = naturalWidth / visibleRatio
+      coordinates.sourceX = 0
+      coordinates.sourceY =
+        (naturalHeight - coordinates.sourceHeight) * verticalPercentage
+    }
+    coordinates.destinationWidthPercentage = 1
+    coordinates.destinationHeightPercentage = 1
+    coordinates.destinationXPercentage = 0
+    coordinates.destinationYPercentage = 0
+  } else {
+    if (imageObjectFit !== 'fill') {
+      console.error(
+        "unexpected 'object-fit' attribute with value '" +
+          imageObjectFit +
+          "' relative to"
+      )
+    }
+    coordinates.sourceWidth = naturalWidth
+    coordinates.sourceHeight = naturalHeight
+    coordinates.sourceX = 0
+    coordinates.sourceY = 0
+    coordinates.destinationWidthPercentage = 1
+    coordinates.destinationHeightPercentage = 1
+    coordinates.destinationXPercentage = 0
+    coordinates.destinationYPercentage = 0
+  }
+
+  console.log(coordinates)
+  coordinates.width =
+    coordinates.destinationWidthPercentage * coordinates.sourceWidth
+  coordinates.height =
+    coordinates.destinationHeightPercentage * coordinates.sourceHeight
+
+  return coordinates
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,63 @@
 import Headline from '#/assets/ui/headline.png'
 import Layout from '#/components/layouts/Layout'
 import { useBreakpointThreshold } from '#/helpers/hooks/useBreakpointThreshold'
+import { calculateObjectFitElement } from '#/helpers/instrinsic-sizes'
 import clsx from 'clsx'
 import type { NextPage } from 'next'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 const Home: NextPage = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
   const mdUp = useBreakpointThreshold('md')
-  const videoName = `/videos/teaser${mdUp ? '' : '-portrait'}`
+  const [hasFirstRendered, setHasFirstRendered] = useState(false)
+  useEffect(() => {
+    setHasFirstRendered(true)
+  }, [])
+
+  const videoSuffix = mdUp ? '' : '-portrait'
+  const videoName = hasFirstRendered ? `/videos/teaser${videoSuffix}` : ''
+
+  const setupCanvasUpdater = useCallback(() => {
+    if (!canvasRef.current || !videoRef.current) return
+    const video = videoRef.current
+    const canvas = canvasRef.current
+    var cw = Math.floor(canvas.clientWidth)
+    var ch = Math.floor(canvas.clientHeight)
+    canvas.width = cw
+    canvas.height = ch
+    const context = canvasRef.current.getContext('2d')
+
+    function updateCanvas() {
+      if (video.paused || video.ended) return false
+      const { width, height, sourceX, sourceY } =
+        calculateObjectFitElement(video)
+      context?.drawImage(
+        video,
+        sourceX ?? 0,
+        sourceY ?? 0,
+        width ?? cw,
+        height ?? ch,
+        0,
+        0,
+        1920,
+        1080
+      )
+      setTimeout(updateCanvas, 20)
+    }
+
+    videoRef.current.addEventListener('play', updateCanvas, false)
+  }, [])
+
+  useEffect(() => {
+    const video = videoRef.current
+    if (!videoName || !video) return
+    video.pause()
+    video.load()
+    video.play()
+
+    setupCanvasUpdater()
+  }, [videoName, setupCanvasUpdater])
 
   return (
     <Layout
@@ -15,25 +66,36 @@ const Home: NextPage = () => {
       mobileFooter
       mobileFooterTitle>
       <div className={clsx('w-full h-screen relative mb-8')}>
-        <video
-          playsInline
-          autoPlay
-          muted
-          loop
+        {videoName && (
+          <video
+            ref={videoRef}
+            playsInline
+            muted
+            loop
+            className={clsx(
+              'absolute w-full h-full top-0 left-0',
+              'object-cover',
+              'z-10'
+            )}>
+            <source src={`${videoName}.webm`} type='video/webm' />
+            <source src={`${videoName}.mp4`} type='video/mp4' />
+          </video>
+        )}
+        <div
           className={clsx(
-            'absolute w-full h-full top-0 left-0',
-            'object-cover'
+            'absolute top-8',
+            'w-full h-full',
+            'blur-xl opacity-80 z-0'
           )}>
-          <source src={`${videoName}.webm`} type='video/webm' />
-          <source src={`${videoName}.mp4`} type='video/mp4' />
-        </video>
+          <canvas className={clsx('w-full h-full')} ref={canvasRef} />
+        </div>
         <div
           className={clsx('absolute w-full h-full', 'bg-black opacity-30')}
         />
         <div
           className={clsx(
             'absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2',
-            'w-full max-w-screen-lg px-12'
+            'w-full max-w-screen-lg px-12 z-10'
           )}>
           <img
             className='w-full'
