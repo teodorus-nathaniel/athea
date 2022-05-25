62225858cb2b201405ce190b3b80c7c477260a32
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-05-25 12:01:58 +0700

    Rename service video

--- src/data/services.ts
+++ src/data/services.ts
@@ -4,23 +4,23 @@ const services: ServiceData[] = [
   {
     title: 'Athea Visuals',
     subtitle: 'Music Video, Fashion Film, Short Film, Documentary',
-    video: '/services/visuals.webm',
+    video: '/services/collective.webm',
   },
   {
     title: 'Athea Collective',
     subtitle:
       'Corporate Video, Web Series, Commercial Video, Brand Identity, Packaging',
-    video: '/services/visuals.webm',
+    video: '/services/collective.webm',
   },
   {
     title: 'Athea Moments',
     subtitle: 'Event Documentation',
-    video: '/services/visuals.webm',
+    video: '/services/collective.webm',
   },
   {
     title: 'Athea Studios',
     subtitle: 'Post Production, Animation, Lyric Video, Motion Graphic',
-    video: '/services/visuals.webm',
+    video: '/services/collective.webm',
   },
 ]
 export default services
